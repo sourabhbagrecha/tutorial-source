7958d2f72b4fe02510616f675b830ee72676e700
Junmin Lee <junminlee@Junmins-MBP.modem>
2020-09-05 01:07:36 +1000

    Finalize hash table example

--- data-structure-golang/hash-table/main.go
+++ data-structure-golang/hash-table/main.go
@@ -2,91 +2,61 @@ package main
 
 import "fmt"
 
+//ArraySize is the size of the hash table array
 const ArraySize = 7
 
-//HashTable will
+// HashTable will hold an array
 type HashTable struct {
-	keyArray [ArraySize]*bucket
+	array [ArraySize]*bucket
 }
 
-// bucket will
+// bucket is a linked list in each slot of the hash table array
 type bucket struct {
 	head *bucketNode
 }
 
-// bucketNode will
+// bucketNode is a linked list node that holds the key
 type bucketNode struct {
-	data string
+	key  string
 	next *bucketNode
 }
 
-//hash will take in a string and return a hash code
-func hash(s string) int {
-	sum := 0
-	for _, v := range s {
-		sum += int(v)
-	}
-	return sum % ArraySize
+//Insert will take in a key and add it to the hash table array
+func (h *HashTable) Insert(key string) {
+	index := hash(key)
+	h.array[index].insert(key)
 }
 
-//Init will create a hash table and initialize the array with empty buckets
-func Init() *HashTable {
-	h := &HashTable{}
-	for i := range h.keyArray {
-		h.keyArray[i] = &bucket{}
-	}
-	return h
-}
+//Search will take in a key and return true if that key is stored in the hash table
+func (h *HashTable) Search(key string) bool {
+	index := hash(key)
+	return h.array[index].search(key)
 
-// Insert will take in a key and insert it in the hash table
-func (h *HashTable) Insert(k string) {
-	index := hash(k)
-	h.keyArray[index].insert(k)
 }
 
 // Delete will take in a key and delete it from the hash table
-func (h *HashTable) Delete(k string) {
-	index := hash(k)
-	h.keyArray[index].delete(k)
+func (h *HashTable) Delete(key string) {
+	index := hash(key)
+	h.array[index].delete(key)
+
 }
 
-// Search will take in a key and return true if that key exists in the hash table
-func (h *HashTable) Search(k string) bool {
-	index := hash(k)
-	return h.keyArray[index].search(k)
-}
-
+// insert will take in a key, create a node with the key and insert the node in the bucket
 func (b *bucket) insert(k string) {
-	newNode := &bucketNode{data: k}
 	if !b.search(k) {
+		newNode := &bucketNode{key: k}
 		newNode.next = b.head
 		b.head = newNode
-		fmt.Println("node added", newNode.data)
 	} else {
-		fmt.Println(k, "is not added because it is already in the list")
+		fmt.Println(k, "already exists")
 	}
 }
 
-func (b *bucket) delete(k string) {
-	if b.head.data == k {
-		b.head = b.head.next
-		return
-	}
-
-	previousNode := b.head
-	for previousNode.next.data != k {
-		if previousNode.next.next == nil {
-			return
-		}
-		previousNode = previousNode.next
-	}
-	previousNode.next = previousNode.next.next
-}
-
+// search will take in a key and return true if the bucket has that key
 func (b *bucket) search(k string) bool {
 	currentNode := b.head
 	for currentNode != nil {
-		if currentNode.data == k {
+		if currentNode.key == k {
 			return true
 		}
 		currentNode = currentNode.next
@@ -94,12 +64,60 @@ func (b *bucket) search(k string) bool {
 	return false
 }
 
+// delete will take in a key and delete the node from the bucket
+func (b *bucket) delete(k string) {
+
+	if b.head.key == k {
+		b.head = b.head.next
+		return
+	}
+
+	previousNode := b.head
+	for previousNode.next != nil {
+		if previousNode.next.key == k {
+			//delete
+			previousNode.next = previousNode.next.next
+		}
+		previousNode = previousNode.next
+	}
+}
+
+// hash
+func hash(key string) int {
+	sum := 0
+	for _, v := range key {
+		sum += int(v)
+	}
+	return sum % ArraySize
+}
+
+// Init will create a bucket in each slot of the hash table
+func Init() *HashTable {
+	result := &HashTable{}
+	for i := range result.array {
+		result.array[i] = &bucket{}
+	}
+	return result
+}
+
 func main() {
 	hashTable := Init()
+	list := []string{
+		"ERIC",
+		"KENNY",
+		"KYLE",
+		"STAN",
+		"RANDY",
+		"BUTTERS",
+		"TOKEN",
+	}
 
-	hashTable.Insert("RANDY")
-	hashTable.Insert("ERIC")
-	hashTable.Insert("KENNY")
-	hashTable.Insert("KENNY")
-	fmt.Println(hashTable.Search("KENNY"))
+	for _, v := range list {
+		hashTable.Insert(v)
+	}
+
+	hashTable.Delete("STAN")
+	fmt.Println("STAN", hashTable.Search("STAN"))
+	fmt.Println("KENNY", hashTable.Search("KENNY"))
+
 }
