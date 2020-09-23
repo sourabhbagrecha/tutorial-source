4f3db18c44185a5a67f9160bcbbaabc1e7fda792
Junmin Lee <junminlee@Junmins-MBP.modem>
2020-09-23 23:16:26 +1000

    Update week 10

---
+++ beginner-golang/tutorial-3/main.go
@@ -0,0 +1,13 @@
+/*
+Package main is an executable package
+that can print hello world on the console
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	// Print Hello World in the console
+	fmt.Println("Hello World")
+}
