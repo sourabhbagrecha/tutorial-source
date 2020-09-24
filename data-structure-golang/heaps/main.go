562d6ecc4c625659fdc17f6eec6fec6e4fe11f13
Junmin Lee <junminlee@Junmins-MBP.modem>
2020-09-25 03:24:59 +1000

    Align with video

--- data-structure-golang/heaps/main.go
+++ data-structure-golang/heaps/main.go
@@ -1,29 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"log"
-)
+import "fmt"
 
-// MaxHeap is a slice that holds the array
+// MaxHeap struct has a slice that holds the array
 type MaxHeap struct {
 	array []int
-}
-
-func parent(i int) int {
-	return (i - 1) / 2
-}
-
-func left(i int) int {
-	return 2*i + 1
-}
-
-func right(i int) int {
-	return 2*i + 2
-}
-
-func (h *MaxHeap) swap(i1, i2 int) {
-	h.array[i1], h.array[i2] = h.array[i2], h.array[i1]
 }
 
 // Insert adds an element to the heap
@@ -32,65 +13,90 @@ func (h *MaxHeap) Insert(key int) {
 	h.maxHeapifyUp(len(h.array) - 1)
 }
 
-func (h *MaxHeap) maxHeapifyUp(i int) {
-
-	for h.array[parent(i)] < h.array[i] {
-		h.swap(parent(i), i)
-		i = parent(i)
-	}
-}
-
-//Extract returns the largest key, and removes it from the heap
+// Extract returns the largest key, and removes it from the heap
 func (h *MaxHeap) Extract() int {
+	extracted := h.array[0]
 	l := len(h.array) - 1
-	if l == 0 {
-		log.Printf("cannot extract because array length is 0")
+	// when the array is empty
+	if len(h.array) == 0 {
+		fmt.Println("cannot extract because array length is 0")
 		return -1
 	}
-	extracted := h.array[0]
+	// take out the last index and put it in the root
 	h.array[0] = h.array[l]
 	h.array = h.array[:l]
+
 	h.maxHeapifyDown(0)
+
 	return extracted
 }
 
-func (h *MaxHeap) maxHeapifyDown(i int) {
+// maxHeapifyUp will heapify from bottom top
+func (h *MaxHeap) maxHeapifyUp(index int) {
+	for h.array[parent(index)] < h.array[index] {
+		h.swap(parent(index), index)
+		index = parent(index)
+	}
+}
+
+// maxHeapifyDown will heapify top to bottom
+func (h *MaxHeap) maxHeapifyDown(index int) {
 
 	lastIndex := len(h.array) - 1
-	l, r := left(i), right(i)
+	l, r := left(index), right(index)
 	childToCompare := 0
 
 	// loop while index has at least one child
 	for l <= lastIndex {
-		if l == lastIndex { // when i only has one child
+		if l == lastIndex { // when left child is the only child
 			childToCompare = l
 		} else if h.array[l] > h.array[r] { // when left child is larger
 			childToCompare = l
 		} else { // when right child is larger
 			childToCompare = r
 		}
-		// compare array value of current index to larger child and swap if current is smaller
-		if h.array[i] < h.array[childToCompare] {
-			h.swap(i, childToCompare)
-			//update loop
-			i = childToCompare
-			l, r = left(i), right(i)
+		// compare array value of current index to larger child and swap if smaller
+		if h.array[index] < h.array[childToCompare] {
+			h.swap(index, childToCompare)
+			index = childToCompare
+			l, r = left(index), right(index)
 		} else {
-			// when the i is finally larger than the larger child
 			return
 		}
 
 	}
+}
 
+// get the parent index
+func parent(i int) int {
+	return (i - 1) / 2
+}
+
+// get the left child index
+func left(i int) int {
+	return 2*i + 1
+}
+
+// get the right child index
+func right(i int) int {
+	return 2*i + 2
+}
+
+// swap keys in the array
+func (h *MaxHeap) swap(i1, i2 int) {
+	h.array[i1], h.array[i2] = h.array[i2], h.array[i1]
 }
 
 func main() {
 	m := &MaxHeap{}
+	fmt.Println(m)
+
 	buildHeap := []int{10, 20, 30, 5, 7, 9, 11, 13, 15, 17}
 	for _, v := range buildHeap {
 		m.Insert(v)
+		fmt.Println(m)
 	}
-	fmt.Println(m)
+
 	for i := 0; i < 5; i++ {
 		m.Extract()
 		fmt.Println(m)
